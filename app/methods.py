31d7678cfbefc2f0b93c3af958a288a20e86f6eb
LucienTriail <lucien.triail@gmail.com>
2023-02-20 19:21:17 +0100

    search works

--- app/methods.py
+++ app/methods.py
@@ -3,7 +3,8 @@ from .models import Book
 
 
 def search_all_fields(q):
-    vector = SearchVector('title', 'author',  'description')
+    vector = SearchVector('title', 'authors',  'description')
     query = SearchQuery(q, search_type='phrase')
     result = Book.objects.annotate(rank=SearchRank(vector, query)).order_by('-rank')
+    print(result[0].authors)
     return result
