b54a7136c92cec1aac367fd26c359d1983570522
LucienTriail <lucien.triail@gmail.com>
2023-02-20 18:12:08 +0100

    plein de modifs

--- app/models.py
+++ app/models.py
@@ -2,22 +2,17 @@ from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.contrib.postgres.indexes import GinIndex
 
+
 # isbn_10 = models.TextField()
 #     isbn_13 = models.TextField()
 
 class Book(models.Model):
-
     title = models.CharField(max_length=255)
-    authors =ArrayField(models.CharField(max_length=255))
-    publishedDate = models.DateField()
-    publisher = models.TextField()
+    authors = ArrayField(models.CharField(max_length=255))
+    publishedDate = models.TextField()
     description = models.TextField()
-
 
     class Meta:
         indexes = [
-            GinIndex(fields=['publishedDate', 'title', 'authors',   'description' ] , name='books_index')
+            GinIndex(fields=['publishedDate', 'title', 'authors', 'description'], name='books_index')
         ]
-
-
-
--- getData.py
+++ getData.py
@@ -5,11 +5,12 @@ import django
 def get_data():
     hey = []
     result = requests.get(
-        'https://www.googleapis.com/books/v1/volumes?q=subject:fantasy&maxResults=40&key=AIzaSyBig4wbXKdDJx6TSzrfimU81obiP_rnPFI')
+        'https://www.googleapis.com/books/v1/volumes?q=subject:&startIndex=0&maxResults=40&key=AIzaSyBig4wbXKdDJx6TSzrfimU81obiP_rnPFI')
 
     truc = result.json()["items"]
-    print(truc["volumeInfo"]["publishedDate"])
     for book in truc:
+        print(book["volumeInfo"]["authors"])
+
         hey.append(Book(title=book["volumeInfo"]["title"], authors=book["volumeInfo"]["authors"],
                         publishedDate=book["volumeInfo"]["publishedDate"])
                    )
