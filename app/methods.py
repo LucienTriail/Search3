73665da23bd1f84aef7d25744aa132c87aaf10b1
LucienTriail <lucien.triail@gmail.com>
2023-02-27 15:21:59 +0100

    10ers resultats

--- app/methods.py
+++ app/methods.py
@@ -1,11 +1,12 @@
 from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
+
 from .models import Book
 
 
 def search_all_fields(q):
     vector = SearchVector('title', 'authors',  'category')
     query = SearchQuery(q, search_type='phrase')
-    result = Book.objects.annotate(rank=SearchRank(vector, query)).order_by('-rank')
+    result = Book.objects.annotate(rank=SearchRank(vector, query)).order_by('-rank')[:10]
     #print(result[0].authors)
     # treeByWord(result, q)
     return result
