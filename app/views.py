9a0b5ebc0bf83fc3c21845a28b165b7a657eb29e
LucienTriail <lucien.triail@gmail.com>
2023-02-27 13:06:13 +0100

    ajout de la route books

--- app/views.py
+++ app/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.views import APIView
 
+from indexer import search_books
 from .serializers import BookSerializer
-from .methods import *
-import requests
 
 
 class Search(APIView):
@@ -12,11 +10,8 @@ class Search(APIView):
     def post(self,request):
         serializer = BookSerializer(data=request.data)
         if request.data is not None:
-            result = search_all_fields(request.data)
-            serializer = BookSerializer(result,many=True)
+            result = search_books(request.data)
+            serializer = BookSerializer(result, many=True)
             return JsonResponse(serializer.data)
         else:
             return JsonResponse(serializer.errors, status=400)
-
-
-
